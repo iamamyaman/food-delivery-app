f06d986e5e4d367f817b49c5f159a9a5a93e2617
Aman <iemamyaman007@gmail.com>
2022-05-02 14:21:59 +0530

    ..

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header =()=>{
+    return(
+        <div className="fixed z-50 w-screen bg-slate-400 p-6 px-16">
+            <div className="hidden md:flex w-full h-full  "></div>
+            <div className="block w-full h-full p-4 "></div>
+        </div>
+    )
+}
+
+export default Header;
